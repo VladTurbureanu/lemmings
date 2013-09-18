a823193eb858ef6ee1dc1ec0127ef32100c14f96
s2288842 <s2288842@fwn-bborg-edu-74-38.fmnsedu.rug.nl>
2013-09-18 11:27:43 +0200

    init

---
+++ src/edu/rug/gogp/patrick/lemmings/Lemmings.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rug.gogp.patrick.lemmings;
+
+/**
+ *
+ * @author s2288842
+ */
+public class Lemmings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
